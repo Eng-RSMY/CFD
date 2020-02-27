fdfd4f4d1d8cb9f2e63dcccd7d1d06a4bc3cf404
alitimer <ali@PAWSEY007-KF.nexus.csiro.au>
2020-02-27 22:43:47 +0800

    contents.rst not found in readthedocs

--- conf.py
+++ conf.py
@@ -53,3 +53,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# contents.rst not found error in readthedocs cloned from github
+master_doc = 'index'
